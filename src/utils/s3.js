3ba73d9530d246a381ab055e1463f2114d7a7726
DouglasMin <douglas0703@daum.net>
2025-02-05 16:01:58 +0900

    fix: s3 access error second attempt

--- src/utils/s3.js
+++ src/utils/s3.js
@@ -1,11 +1,7 @@
 const { S3Client, ListObjectsV2Command } = require('@aws-sdk/client-s3');
 
 const s3Client = new S3Client({
-    region: process.env.AWS_REGION || 'ap-northeast-2',
-    credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-    }
+    region: process.env.AWS_REGION || 'ap-northeast-2'
 });
 
 /**
